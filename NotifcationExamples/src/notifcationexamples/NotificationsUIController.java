8591399b2f30ba4ff2ba2f6312fe86e3d3e6c588
asp6f8 <apsynthes@gmail.com>
2018-02-16 21:51:38 -0600

    Added files to master branch
    All final files are now added to repo.

---
+++ NotifcationExamples/src/notifcationexamples/NotificationsUIController.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package notifcationexamples;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import static java.lang.Thread.State.TERMINATED;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import taskers.*;
+
+/**
+ * FXML Controller class
+ *
+ * @author dalemusser
+ */
+public class NotificationsUIController implements Initializable, Notifiable {
+
+    @FXML
+    private TextArea textArea;
+    @FXML
+    private Button task1Button;
+    @FXML
+    private Button task2Button;
+    @FXML
+    private Button task3Button;
+    
+    private Task1 task1;
+    private Task2 task2;
+    private Task3 task3;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+    
+    public void start(Stage stage) {
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            public void handle(WindowEvent we) {
+                if (task1 != null) task1.end();
+                if (task2 != null) task2.end();
+                if (task3 != null) task3.end();
+            }
+        });
+    }
+    
+    @FXML
+    public void startTask1(ActionEvent event) {
+        if (task1 == null || task1.getState() == TERMINATED) {
+            System.out.println("Start Task 1");
+            task1Button.setText("End Task 1");
+            task1 = new Task1(2147483647, 1000000);
+            task1.setNotificationTarget(this);
+            task1.start();
+        } else {
+            task1Button.setText("Start Task 1");
+            task1.end();
+        }
+    }
+    
+    @Override
+    public void notify(String message) {
+        if (message.equals("Task1 done.")) {
+            task1 = null;
+            task1Button.setText("Start Task 1");
+        }
+        textArea.appendText(message + "\n");
+    }
+    
+    @FXML
+    public void startTask2(ActionEvent event) {
+        if (task2 == null || task2.getState() == TERMINATED) {
+            System.out.println("Start Task 2");
+            task2Button.setText("End Task 2");
+            task2 = new Task2(2147483647, 1000000);
+            task2.setOnNotification((String message) -> {
+                textArea.appendText(message + "\n");
+            });
+            
+            task2.start();
+        } else {
+            task2Button.setText("Start Task 2");
+            task2.end();
+        }     
+    }
+    
+    @FXML
+    public void startTask3(ActionEvent event) {
+        if (task3 == null || task3.getState() == TERMINATED) {
+            System.out.println("Start Task 3");
+            task3Button.setText("End Task 3");
+            task3 = new Task3(2147483647, 1000000);
+            // this uses a property change listener to get messages
+            task3.addPropertyChangeListener((PropertyChangeEvent evt) -> {
+                textArea.appendText((String)evt.getNewValue() + "\n");
+            });
+            
+            task3.start();
+        } else {
+            task3Button.setText("Start Task 3");
+            task3.end();
+        }
+    } 
+}
